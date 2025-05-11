aa888d5889d7687e2163586c971921b5cc45383a
Mahil Sardana <sardanamahil@gmail.com>
2025-05-11 19:37:28 +0530

    c3

--- src/main/java/PodTest.java
+++ src/main/java/PodTest.java
@@ -2,5 +2,13 @@
 public class PodTest {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        //sum();
+        //or
+        PodTest.sum();
+        }
+
+    public static void sum(){
+        int c=5+6;
+        System.out.println("sum is "+c);
     }
 }
