aa2fe3e27500f7c96cc7c66d6996e1a67884bc63
Mahil Sardana <sardanamahil@gmail.com>
2025-05-24 20:35:12 +0530

    conditional Statement

---
+++ src/main/java/LoopingDemo.java
@@ -0,0 +1,13 @@
+public class LoopingDemo {
+    public static void main(String[] args) {
+        //calling static method-class name.method name
+        LoopingDemo.forDemo();
+    }
+    public static void forDemo() {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Akhil Jain "+i);
+        }
+    }
+}
+
