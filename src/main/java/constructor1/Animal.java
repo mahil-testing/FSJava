2ab27ab7b972b88e7ee2c6e6861c2e1acd143df2
Mahil Sardana <sardanamahil@gmail.com>
2025-05-31 20:09:15 +0530

    day 11 changes

---
+++ src/main/java/constructor1/Animal.java
@@ -0,0 +1,13 @@
+package constructor1;
+
+public class Animal {
+    String name;
+    int id;
+
+    public Animal(){
+        System.out.println("I am an animal");
+    }
+    public void getDetails(){
+        System.out.println("Name is :"+name+", id is: "+id);
+    }
+}
---
+++ src/main/java/constructor1/Jungle.java
@@ -0,0 +1,8 @@
+package constructor1;
+
+public class Jungle {
+    public static void main(String[] args) {
+        Animal lion =new Animal();
+        System.out.println(lion.name);
+    }
+}
